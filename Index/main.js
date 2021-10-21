2d800a7b1d9afbcd9fd6f7667fec5eaf2b395a3c
JuanJoCode <90096213+JuanJoCode@users.noreply.github.com>
2021-10-21 18:47:21 -0500

    Add files via upload

---
+++ Index/main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards',{delay:500});
+ScrollReveal().reveal('.cards-banner-one', {delay:500});
+ScrollReveal().reveal('.cards-banner-two', {delay:300});
+ScrollReveal().reveal('.showcase1', {delay:300});
+ScrollReveal().reveal('.showcase2', {delay:300});
+ScrollReveal().reveal('.showcase3', {delay:300});
+ScrollReveal().reveal('.showcase4', {delay:300});
+ScrollReveal().reveal('.showcase5', {delay:300});
